4b76d3d49976e06b016b861c316566dbf5a72896
Sitong-Andy <sitongfeng@gmail.com>
2022-03-18 11:45:21 -0700

    fix: update PerdleGame format fix: rename file name

--- PerdleGame.py
+++ PerdleGame.py
@@ -1,4 +1,5 @@
-from pinyin2list import convert_pinyin_to_list
+from pinyin_to_list import convert_pinyin_to_list
+
 
 chengyu_path = open("chengyu_introText.csv", "r", encoding="utf-8")
 chengyu_set = chengyu_path.read().split("\n")
@@ -15,14 +16,16 @@ for idx in range(len(chengyu_set) // 2):
 
 # word = [-1, -1, -1, -1]
 # tone = [-1, -1, -1, -1]
-# correct_pos = [[[]], [], [], []]
+# correct_pos = [[[]], [[]], [[]], [[]]]
 # wrong_pos = [[[]], [[]], [[]], [[]]]
 
 
 word = [-1, -1, -1, -1]
 tone = [-1, -1, -1, -1]
-correct_pos = [[[]], [], [], []]
+correct_pos = [[[]], [[]], [[]], [[]]]
 wrong_pos = [[[]], [[]], [[]], [[]]]
+
+
 possible_result = []
 for c in chengyu_dict:
     is_match = True
@@ -30,9 +33,11 @@ for c in chengyu_dict:
     for i in range(len(word)):
         if word[i] != -1 and c[i][2] != word[i]:
             is_match = False
+
     for i in range(len(tone)):
         if tone[i] != -1 and c[i][1] != tone[i]:
             is_match = False
+
     for i in range(len(correct_pos)):
         for j in range(len(correct_pos[i])):
             if (
@@ -41,6 +46,7 @@ for c in chengyu_dict:
                 and correct_pos[i][j][0] != c[i][0][correct_pos[i][j][1]]
             ):
                 is_match = False
+
     for i in range(len(wrong_pos)):
         for j in range(len(wrong_pos[i])):
             if (
@@ -49,6 +55,9 @@ for c in chengyu_dict:
                 and wrong_pos[i][j][0] == c[i][0][wrong_pos[i][j][1]]
             ):
                 is_match = False
+
     if is_match:
         possible_result.append(cur_chengyu)
+
+
 print(possible_result)
---
+++ pinyin_to_list.py
@@ -0,0 +1,42 @@
+from dict import (
+    mapConstTone2ToneConst,
+    mapVowelVowelTone2VowelToneVowel,
+    mapVowelTone2Unicode,
+)
+
+
+def ConvertToneNumbersPinyin(lineIn):
+    assert type(lineIn) is str
+    lineOut = lineIn
+
+    for x, y in mapVowelTone2Unicode.items():
+        lineOut = lineOut.replace(y, x).replace(y.upper(), x.upper())
+
+    for x, y in mapVowelVowelTone2VowelToneVowel.items():
+        lineOut = lineOut.replace(y, x).replace(y.upper(), x.upper())
+
+    for x, y in mapConstTone2ToneConst.items():
+        lineOut = lineOut.replace(y, x).replace(y.upper(), x.upper())
+
+    return lineOut.replace("Ü", "V").replace("ü", "v")
+
+
+def convert_pinyin_to_list(pinyin):
+    lineOut = ConvertToneNumbersPinyin(pinyin)
+    result = []
+    temp = []
+    pinyin_list = lineOut.split(" ")
+    for i in pinyin_list:
+        num = 0
+        for j in i:
+            if j.isdigit():
+                num = int(j)
+            elif j.isalpha():
+                temp.append(j)
+        result.append([temp, num])
+        temp = []
+    return result
+
+
+# Test
+# print(convert_pinyin_to_list("ēn bù fàng zhai"))

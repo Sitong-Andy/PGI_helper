2d90064c28e443f3a0c56d5d24858602a468ba88
Sitong-Andy <sitongfeng@gmail.com>
2022-03-18 11:46:23 -0700

    feat: added HandleGame helper to solve handle

---
+++ HandleGame.py
@@ -0,0 +1,81 @@
+from vowel_consonant_to_list import convert_vowel_consonant_to_list
+
+
+chengyu_path = open("chengyu_introText.csv", "r", encoding="utf-8")
+chengyu_set = chengyu_path.read().split("\n")
+chengyu_dict = []
+for idx in range(len(chengyu_set) // 2):
+    temp = []
+    chengyu = chengyu_set[idx * 2]
+    duyin = chengyu_set[idx * 2 + 1]
+    convert_cv = convert_vowel_consonant_to_list(duyin)
+    convert_cv.append(chengyu)
+    chengyu_dict.append(convert_cv)
+
+
+# tone = [-1, -1, -1, -1]
+# correct_con = ["", "", "", ""]
+# correct_vo = ["", "", "", ""]
+# con_not_in_word = []
+# vo_not_in_word = []
+# poss_con_in_word = []
+# poss_vo_in_word = []
+
+
+tone = [-1, -1, -1, -1]
+correct_con = ["", "", "", ""]
+correct_vo = ["", "", "", ""]
+con_not_in_word = []
+vo_not_in_word = []
+poss_con_in_word = []
+poss_vo_in_word = []
+
+
+possible_result = []
+for c in chengyu_dict:
+    is_match = True
+    cur_chengyu = c[4]
+    for i in range(len(tone)):
+        if tone[i] != -1 and c[i][2] != tone[i]:
+            is_match = False
+
+    for i in range(len(correct_con)):
+        if correct_con[i] and correct_con[i] != c[i][0]:
+            is_match = False
+
+    for i in range(len(correct_vo)):
+        if correct_vo[i] and correct_vo[i] != c[i][1]:
+            is_match = False
+
+    for i in con_not_in_word:
+        for j in range(4):
+            if i == c[j][0]:
+                is_match = False
+
+    for i in vo_not_in_word:
+        for j in range(4):
+            if i == c[j][1]:
+                is_match = False
+
+    poss_in_word = True
+    for i in poss_con_in_word:
+        temp = False
+        for j in range(4):
+            if i == c[j][0]:
+                temp |= True
+        poss_in_word &= temp
+
+    for i in poss_vo_in_word:
+        temp = False
+        for j in range(4):
+            if i == c[j][1]:
+                temp |= True
+        poss_in_word &= temp
+    if not poss_in_word:
+        is_match = False
+
+    if is_match:
+        possible_result.append(cur_chengyu)
+
+
+print(possible_result)
---
+++ vowel_consonant_to_list.py
@@ -0,0 +1,39 @@
+from pinyin_to_list import ConvertToneNumbersPinyin
+from dict import consontant_dict, vowel_dict
+
+
+def get_consontant(pinyin):
+    for i in consontant_dict:
+        if pinyin.startswith(i):
+            return i
+    return ""
+
+
+def get_vowel(pinyin):
+    consontant = get_consontant(pinyin)
+    vowel = pinyin[len(consontant) :]
+    if vowel not in vowel_dict:
+        return ""
+    return vowel
+
+
+def convert_vowel_consonant_to_list(pinyin):
+    result = []
+    lineOut = ConvertToneNumbersPinyin(pinyin)
+    pinyin_list = lineOut.split(" ")
+    for i in pinyin_list:
+        num = 0
+        py = ""
+        for j in i:
+            if j.isdigit():
+                num = int(j)
+            elif j.isalpha():
+                py += j
+        con = get_consontant(py)
+        vo = get_vowel(py)
+        result.append([con, vo, num])
+    return result
+
+
+# Test
+# print(convert_vowel_consonant_to_list("zhàng lǚ xiāng cóng"))
